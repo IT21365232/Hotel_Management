1b426c3761c1a161b283f32b64cf4484cc09db23
Binuda Dewhan <it21365232@my.sliit.lk>
2023-04-29 15:33:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import {BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import AddRoom from './Rooms/AddRoom';
+import EditRoom from "./Rooms/EditRoom";
+import ViewRoom from "./Rooms/ViewRoom";
+import Home1 from "./pages/Home1";
+import ViewRooms from "./pages/ViewRooms";
+import BoardRooms from "./pages/BoardRooms";
+import CheckUp from "./pages/CheckUp";
+import RoomService from "./pages/RoomService";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Router>
+       <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home1/>}/>
+          <Route exact path="/addroom" element={<AddRoom/>}/>
+          <Route exact path="/viewrooms" element={<ViewRooms/>}/>
+          <Route exact path="/checkup" element={<CheckUp/>}/>
+          <Route exact path="/roomservice" element={<RoomService/>}/>
+          <Route exact path="/editroom/:rid" element={<EditRoom/>}/>
+          <Route exact path="/viewroom/:rid" element={<ViewRoom/>} />
+          <Route exact path="/boardrooms" element={<BoardRooms/>}/>
+        </Routes>  
+      </Router>
+
+      
+
     </div>
   );
 }
-
 export default App;
